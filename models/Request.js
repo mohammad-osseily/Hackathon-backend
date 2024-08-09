5aaaa7ff4690306c5b041ba0756ff247154462a9
MohammadOsseily <mohammad.osseily2@gmail.com>
2024-08-10 00:00:52 +0300

    added request model

---
+++ models/Request.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const requestSchema = new mongoose.Schema(
+  {
+    user_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Request", requestSchema);
