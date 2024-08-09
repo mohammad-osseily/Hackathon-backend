ce05d2df49bbae8734a42bae16d719eea5bcbad5
MohammadOsseily <mohammad.osseily2@gmail.com>
2024-08-10 00:13:36 +0300

    installed dependencies added protected user for requestroute

--- routes/requestRoutes.js
+++ routes/requestRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { createRequest } from "./../controllers/requestController";
 
+import { protect } from "../middleware/protectRoutes.js";
+
 const router = express.Router();
 
-router.post("/", createRequest);
+router.post("/", protect, createRequest);
 
 export default router;
