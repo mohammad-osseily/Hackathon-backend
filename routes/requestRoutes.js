859b95d70fc4bad3bf96508497d0699eb7fd1a02
MohammadOsseily <mohammad.osseily2@gmail.com>
2024-08-10 00:04:47 +0300

    added createRequest route

---
+++ routes/requestRoutes.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { createRequest } from "./../controllers/requestController";
+
+const router = express.Router();
+
+router.post("/", createRequest);
+
+export default router;
