930c9afcd284a1699041513d655884825f8ef168
MohammadOsseily <mohammad.osseily2@gmail.com>
2024-08-11 02:59:40 +0300

    adding app date route

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ import databaseConnection from "./connection.js";
 import userRoutes from "./routes/userRoutes.js";
 import aiRequestRoutes from "./routes/aiRequestRoutes.js";
 import aiResponsetRoutes from "./routes/aiResponseRoutes.js";
+import appDataRoutes from "./routes/appDataRoutes.js";
+
 dotenv.config();
 
 const server = express();
@@ -27,6 +29,7 @@ server.use((req, res, next) => {
 server.use("/users", userRoutes);
 server.use("/aiRequest", aiRequestRoutes);
 server.use("/aiResponse", aiResponsetRoutes);
+server.use("/appData", appDataRoutes);
 
 server.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
