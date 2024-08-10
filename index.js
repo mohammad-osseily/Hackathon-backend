5c45e6996a31f5751420bff0e4cb78a193615145
MohammadOsseily <mohammad.osseily2@gmail.com>
2024-08-10 14:07:19 +0300

    updated name typos

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 import express, { json } from "express";
 import dotenv from "dotenv";
 import databaseConnection from "./connection.js";
-import userRoutes from './routes/userRoutes.js'
-import aiRequestRoutes from './routes/requestRoutes.js'
-import aiResponsetRoutes from './routes/aiResponseRoutes.js'
-
-
+import userRoutes from "./routes/userRoutes.js";
+import aiRequestRoutes from "./routes/aiRequestRoutes.js";
+import aiResponsetRoutes from "./routes/aiResponseRoutes.js";
 
 const server = express();
 dotenv.config();
 server.use(express.json());
 
-server.use('/users', userRoutes)
-server.use('/aiRequest', aiRequestRoutes)
-server.use('/aiResponse', aiResponsetRoutes)
-
+server.use("/users", userRoutes);
+server.use("/aiRequest", aiRequestRoutes);
+server.use("/aiResponse", aiResponsetRoutes);
 
 server.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
