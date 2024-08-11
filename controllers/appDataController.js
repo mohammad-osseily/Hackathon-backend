1b301a427bb9ad3cc9e93acab982eaea11475c9a
Ali-Hadi-Saker <alihady530@gmail.com>
2024-08-11 19:11:21 +0300

    adding function to update apps and reviews collections automatically

--- controllers/appDataController.js
+++ controllers/appDataController.js
@@ -49,45 +49,44 @@ export const getApps = async (req, res) => {
     };
 
     export const getReviewByAppName = async (req, res) => {
-        const { page = 1, limit = 20 } = req.query; // Default to page 1, limit 20
-      
+        const { page = 1, limit = 20 } = req.query; // Default to page 1, limit 20      
         try {
-          const { appName } = req.params;
-      
-          const currentPage = parseInt(page, 10);
-          const perPage = parseInt(limit, 10);
-          const skip = (currentPage - 1) * perPage;
-      
-          // Find the document containing reviews for the specific app
-          const appDocument = await appReview.findOne({ [appName]: { $exists: true } }).lean();
-      
-          if (!appDocument || !appDocument[appName]) {
-            return res.status(404).json({ message: `No reviews found for app: ${appName}` });
-          }
-      
-          const reviews = appDocument[appName];
-          const total = reviews.length;
-          const paginatedReviews = reviews.slice(skip, skip + perPage);
-      
-          const lastPage = Math.ceil(total / perPage);
-          const nextPage = currentPage < lastPage ? currentPage + 1 : null;
-          const prevPage = currentPage > 1 ? currentPage - 1 : null;
-      
-          res.json({
-            data: paginatedReviews,
-            total,
-            per_page: perPage,
-            current_page: currentPage,
-            last_page: lastPage,
-            next_page_url: nextPage
-              ? `${req.protocol}://${req.get("host")}${req.baseUrl}${req.path}?page=${nextPage}&limit=${perPage}`
-              : null,
-            prev_page_url: prevPage
-              ? `${req.protocol}://${req.get("host")}${req.baseUrl}${req.path}?page=${prevPage}&limit=${perPage}`
-              : null,
-          });
-        } catch (error) {
-          res.status(500).json({ message: "Error fetching paginated reviews: " + error.message });
-        }
-      };
+            const { appName } = req.params;
+        
+            const currentPage = parseInt(page, 10);
+            const perPage = parseInt(limit, 10);
+            const skip = (currentPage - 1) * perPage;
+        
+            // Find the document containing reviews for the specific app
+            const appDocument = await appReview.findOne({ [appName]: { $exists: true } }).lean();
+        
+            if (!appDocument || !appDocument[appName]) {
+                return res.status(404).json({ message: `No reviews found for app: ${appName}` });
+            }
+        
+            const reviews = appDocument[appName];
+            const total = reviews.length;
+            const paginatedReviews = reviews.slice(skip, skip + perPage);
+        
+            const lastPage = Math.ceil(total / perPage);
+            const nextPage = currentPage < lastPage ? currentPage + 1 : null;
+            const prevPage = currentPage > 1 ? currentPage - 1 : null;
+        
+            res.json({
+                data: paginatedReviews,
+                total,
+                per_page: perPage,
+                current_page: currentPage,
+                last_page: lastPage,
+                next_page_url: nextPage
+                ? `${req.protocol}://${req.get("host")}${req.baseUrl}${req.path}?page=${nextPage}&limit=${perPage}`
+                : null,
+                prev_page_url: prevPage
+                ? `${req.protocol}://${req.get("host")}${req.baseUrl}${req.path}?page=${prevPage}&limit=${perPage}`
+                : null,
+            });
+            } catch (error) {
+            res.status(500).json({ message: "Error fetching paginated reviews: " + error.message });
+            }
+        };
       
--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 import express, { json } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import cron from "node-cron"
 
 import databaseConnection from "./connection.js";
 import userRoutes from "./routes/userRoutes.js";
 import aiRequestRoutes from "./routes/aiRequestRoutes.js";
 import aiResponsetRoutes from "./routes/aiResponseRoutes.js";
 import appDataRoutes from "./routes/appDataRoutes.js";
+import { saveCSVData } from "./data/importCSVData.js";
+import { saveReviewsCSV } from "./data/importCSVReviews.js";
 
 dotenv.config();
+
+//function to update apps and appReviews collections at midnight 
+function importDataAndUpdateDB() {
+  console.log('Running the import and update process...');
+  
+
+  saveCSVData()  
+  saveReviewsCSV()
+}
+cron.schedule('09 19 * * *', () => {
+  console.log('Cron job started at midnight');
+  importDataAndUpdateDB();
+});
 
 const server = express();
 
