fd04e95e4c48c8da524098e1f9b85b9dd1d18a31
Ali-Hadi-Saker <alihady530@gmail.com>
2024-08-12 01:09:57 +0300

    updating apiRequest to send request for ml model

--- controllers/aiRequestController.js
+++ controllers/aiRequestController.js
@@ -50,31 +50,31 @@ export const createRequest = async (req, res) => {
     await newRequest.save();
 
     // Format the data to send to the ML model (Uncomment and use when needed)
-    // const formattedData = {
-    //   Android_Ver: androidVer,
-    //   Size: size,
-    //   Price: price,
-    //   Category_encoded: categoryEncoded,
-    //   Type_Free: typeFree,
-    //   Type_Paid: typePaid,
-    //   Content_Ratings_Adults_only_18: contentRatingsAdultsOnly18,
-    //   Content_Ratings_Everyone: contentRatingsEveryone,
-    //   Content_Ratings_Everyone_10: contentRatingsEveryone10,
-    //   Content_Ratings_Mature_17: contentRatingsMature17,
-    //   Content_Ratings_Teen: contentRatingsTeen,
-    //   Content_Ratings_Unrated: contentRatingsUnrated,
-    //   last_updated_year: lastUpdatedYear,
-    //   last_updated_month: lastUpdatedMonth,
-    // };
+    const formattedData = {
+      Android_Ver: androidVer,
+      Size: size,
+      Price: price,
+      Category_encoded: categoryEncoded,
+      Type_Free: typeFree,
+      Type_Paid: typePaid,
+      Content_Ratings_Adults_only_18: contentRatingsAdultsOnly18,
+      Content_Ratings_Everyone: contentRatingsEveryone,
+      Content_Ratings_Everyone_10: contentRatingsEveryone10,
+      Content_Ratings_Mature_17: contentRatingsMature17,
+      Content_Ratings_Teen: contentRatingsTeen,
+      Content_Ratings_Unrated: contentRatingsUnrated,
+      last_updated_year: lastUpdatedYear,
+      last_updated_month: lastUpdatedMonth,
+    };
 
     // Send the formatted data to the ML model (Uncomment and use when needed)
-    // const mlEndpointUrl = 'https://your-ml-model-endpoint.com/predict'; 
-    // const mlResponse = await axios.post(mlEndpointUrl, formattedData);
+    const mlEndpointUrl = 'http://localhost:7500/predict/rating'; 
+    const mlResponse = await axios.post(mlEndpointUrl, formattedData);
 
     res.status(201).json({
       message: "Request created successfully",
       newRequest,
-      // mlResponse: mlResponse.data, // Uncomment when using the ML model
+      mlResponse: mlResponse.data, // Uncomment when using the ML model
     });
   } catch (error) {
     res.status(500).json({ error: error.message });
