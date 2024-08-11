59e92e38a18013f67110af400d26c24c87ccda91
Ali-Hadi-Saker <alihady530@gmail.com>
2024-08-11 13:24:51 +0300

    pushing changes

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import express, { json } from "express";
 import dotenv from "dotenv";
 import databaseConnection from "./connection.js";
-import userRoutes from "./routes/user.routes.js";
-import aiRequestRoutes from "./routes/aiRequest.routes.js";
-import aiResponsetRoutes from "./routes/aiResponse.routes.js";
-import appDataRoutes from "./routes/appData.routes.js"
+import userRoutes from "./routes/userRoutes.js";
+import aiRequestRoutes from "./routes/aiRequestRoutes.js";
+import aiResponsetRoutes from "./routes/aiResponseRoutes.js";
+import appDataRoutes from "./routes/appDataRoutes.js"
 
 const server = express();
 dotenv.config();
